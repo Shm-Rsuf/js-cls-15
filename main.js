0f2229f7023910953acc365e0caf355c3acda5d5
shm_usuf <shmusuf@gmail.com>
2023-03-14 09:39:55 +0600

    spred, rest, short circuit, nullish, for of, sets added

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
-'use strict';
+"use strict";
 
 /*property of restaurent*/
 const openingHours = {
   sun: {
     open: 10,
-    close: 22
+    close: 22,
   },
   fri: {
     open: 12,
-    close: 20
+    close: 20,
   },
   wed: {
     open: 8,
-    close: 20
-  }
-}
+    close: 20,
+  },
+};
 
 const restaurant = {
-  name: 'Burger House',
-  location: 'Puran Bogura, Dhaka,Bangladesh',
-  category: ['Italian', 'Thailand', 'Indian', 'Local', 'Organic'],
+  name: "Burger House",
+  location: "Puran Bogura, Dhaka,Bangladesh",
+  category: ["Italian", "Thailand", "Indian", "Local", "Organic"],
   startMenu: [
-    'Chicken Cheese Burger',
-    'Beef Burger',
-    'Garlic Bread',
-    'Italian Spicy Pasta'
+    "Chicken Cheese Burger",
+    "Beef Burger",
+    "Garlic Bread",
+    "Italian Spicy Pasta",
   ],
-  mainMenu: ['Burger', 'Pasta', 'Pizza', 'French Fries'],
+  mainMenu: ["Burger", "Pasta", "Pizza", "French Fries"],
 
   openingHours,
   order: function (startInd, mainInd) {
     return [this.startMenu[startInd], this.mainMenu[mainInd]];
   },
-  orderDelivery: function ({ startIndex = 0, mainIndex = 0, time = '10:00pm', address = 'Unknown' }) {
-    console.log(`${this.startMenu[startIndex]} & ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+  orderDelivery: function ({
+    startIndex = 0,
+    mainIndex = 0,
+    time = "10:00pm",
+    address = "Unknown",
+  }) {
+    console.log(
+      `${this.startMenu[startIndex]} & ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
   },
   burgerOrder: function (ing1, ing2, ing3) {
-    console.log(`Here is your is burger with ${ing1} , ${ing2} & ${ing3}`)
+    console.log(`Here is your is burger with ${ing1} , ${ing2} & ${ing3}`);
   },
   orderPizz: function (mainIng, ...other) {
     console.log(mainIng, other);
-  }
-}
-
+  },
+};
 
 ////////////////////////////////////////
 /*Rest Pattern And Parameters  */
@@ -53,29 +59,54 @@ const restaurant = {
 // const arr = [10, 2, ...[3, 4]];
 // const [a, b, c, d] = arr;
 
+// const myArr1 = [1, 2, 3, 5, 10];
+// const myArr2 = [11, 22, 3, 5, 10];
+
+// const myArr3 = [...myArr1, ...myArr2];
+// const [fe, , td, , last] = myArr1;
+// console.log(myArr3);
+// console.log(fe, td, last);
+
 // console.log(a + b * c - d);
 
 /* Rest Pattern---কাজ করে সমান চিনহের বাম দিকে এবং সবার শেষে।*/
 ///////////////////////////////////////////////////////////////////
 
 // const arr2 = [1, 2, 3, 4, 5, 6, 7];
-// const [x, y, ...all] = arr2;
+// const [one, , three, ...rest] = arr2;
+// console.log(one, three, rest);
 
-// const someName = ['Salman', 'Rahat', 'Evan', 'Jony', 'Priti'];
-// const [s, , r, ...others] = someName;
+// const someName = ["Salman", "Rahat", "Evan", "Jony", "Priti"];
+// const [s, , e, , p] = someName;
+// const [, r, ...names] = someName;
+// console.log(r, names);
+// console.log(s, e, p);
 
 // console.log(all);
 // console.log(s, r, others);
 
-// const [checkenBurger, beefBurger, , ...allOthers] = [...restaurant.startMenu, ...restaurant.mainMenu];
-
-// console.log(checkenBurger, beefBurger, allOthers)
+// const [checkenBurger, beefBurger, , ...allOthers] = [
+//   ...restaurant.startMenu,
+//   ...restaurant.mainMenu,
+// ];
+// const [italian, ...otherCategories] = restaurant.category;
+// console.log(italian, otherCategories);
+// console.log(checkenBurger, beefBurger, allOthers);
 
 /*Objects*/
 /////////////////////
+// const teacher = {
+//   name: "Mahabub Alom",
+//   age: 35,
+//   cls: ["six", "seven", "eight", "nine", "ten"],
+// };
 
-// const { fri: friday, ...otherDays } = restaurant.openingHours;
-// console.log(friday, otherDays);
+// const { name, cls } = teacher;
+
+// console.log(name, cls);
+
+// const { fri, ...otherHours } = restaurant.openingHours;
+// console.log(fri, otherHours);
 
 /*Rest Parameter--যখন ফাংশরের ভিতরে ব্যবহার করা তখন তাকে Rest Parameter বলে।*/
 ////////////////////////////////////////////////////////////////////////////////
@@ -123,11 +154,11 @@ const restaurant = {
 // const malik = restaurant.malik ?? 20;
 // console.log(malik)
 
-/*For Of Loop:: Use of Array */
+/*For Of Loop:use for iterable:এর ভেতরে কোন index থাকে না বাইরে থেকে আনতে হয় entries() ফাংশন ব্যবহার করে। */
 ///////////////////////////////
 // const arr4 = [1, 2, 4, 5, 6, 7, 8, 9, 10];
-// for (const element of arr4) {
-//   console.log(element);
+// for (const [key, value] of arr4.entries()) {
+//   console.log(key, value);
 // }
 
 // const menu = [...restaurant.startMenu, ...restaurant.mainMenu];
@@ -148,8 +179,10 @@ const restaurant = {
 
 /*Objects Itarator*/
 ////////////////////////////////
-// const objProperty = Object.keys(restaurant.openingHours);
-// console.log(objProperty);
+const properties = Object.keys(restaurant.openingHours);
+for (const [index, property] of properties.entries()) {
+  console.log(`${index + 1}: ${property}`);
+}
 
 // for (const day of objProperty) {
 //   console.log(day);
@@ -170,7 +203,15 @@ const restaurant = {
 /*-----------sets------------*/
 //////////////////////////////////////
 
-const food = ['burger', 'pizza', 'mattan biriyani', 'pasta', 'noodles', 'pasta', 'burger'];
+const food = [
+  "burger",
+  "pizza",
+  "mattan biriyani",
+  "pasta",
+  "noodles",
+  "pasta",
+  "burger",
+];
 const uniqueFood = [...new Set(food)];
 
 const myArr = [10, 11, 10, 15, 20, 11, 25, 20, 15];
